b5da9eaf4047ce33ac71068c3e6d43a9681313ee
ZeyronJ <93349366+ZeyronJ@users.noreply.github.com>
2024-09-08 04:02:46 -0300

    version 1.2.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/documents/oficiales',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
+};
