795c4152a3abe86499e1ad0dea2c5350d081239f
ZeyronJ <93349366+ZeyronJ@users.noreply.github.com>
2024-09-08 02:41:30 -0300

    demo 1.1.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      // {
+      //   source: '/api/documents', // Endpoint de todos los documentos
+      //   headers: [
+      //     {
+      //       key: 'Cache-Control',
+      //       value: 'no-store, max-age=0', // Desactiva el caché para esta ruta
+      //     },
+      //   ],
+      // },
+      {
+        source: '/api/documents/oficiales', // Endpoint de documentos oficiales
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0', // Desactiva el caché para esta ruta
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
